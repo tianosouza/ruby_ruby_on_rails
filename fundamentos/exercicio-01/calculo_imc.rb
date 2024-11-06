ce87e0917f3aa06d6366e4def8275f52f5975aaf
Cristiano Souza <ltianosouzal@outlook.com>
2024-11-06 20:43:02 -0300

    Aula 05

--- fundamentos/exercicio-01/calculo_imc.rb
+++ fundamentos/exercicio-01/calculo_imc.rb
@@ -5,11 +5,20 @@
 
     Formula IMG = peso / (altura * altura)
 =end
-
+puts "Ctegorias IMC"
+puts "Abaixo de 16.5: peso muito abaixo do normal"
+puts "Abaixo de 18.5 Peso abaixo do normal"
+puts "18.5 - 24.99 Peso normal"
+puts "25 - 29.99 Pré-obeso"
+puts "30 - 34.99 Obesidade calsse I"
+puts "35 - 39.99 Obesidade calsse II"
+puts "Acima de 40 Obesidade calsse III"
 
 puts "Informe seu peso: "
 peso = gets.chomp.to_f
 puts "Informe sua altura: "
 altura = gets.chomp.to_f
-imc = peso / (altura * altura)
+imc = peso / (altura / 100) ** 2
 puts "Seu IMC é: #{imc.round(2)}"
+
+
