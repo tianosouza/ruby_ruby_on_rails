08e5708bc372ef23afac6d59d3b2540678446ae6
Cristiano Souza <ltianosouzal@outlook.com>
2024-11-06 07:46:39 -0300

    Aula 01

---
+++ fundamentos/1-hello.rb
@@ -0,0 +1,14 @@
+# Metodos de impressão
+
+# Mesma linha
+print "Hello World"
+print "Olá Mundo\n"
+
+# Com quebra de linha
+puts "Hello Word"
+puts "Olá Mundo"
+
+=begin
+  Comentários muilti
+  linhas
+=end
