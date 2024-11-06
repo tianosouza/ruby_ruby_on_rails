b561c9d43c0feb0b1e15cbb1c9e166dbc5fc57f4
Cristiano Souza <ltianosouzal@outlook.com>
2024-11-06 07:58:53 -0300

    Aula 02

--- fundamentos/2-tipos.rb
+++ fundamentos/2-tipos.rb
@@ -5,5 +5,6 @@ year_launch = 2009
 price = 100.00
 plan_included = false
 
+# Identificando o tipo de dados
 puts name, year_launch
 puts "name: #{name.class}, year: #{year_launch.class}, price: #{price.class}, plan: #{plan_included.class}"
