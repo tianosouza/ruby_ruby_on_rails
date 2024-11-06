32c972a606904b9312dd14aff3efd67aef624462
Cristiano Souza <ltianosouzal@outlook.com>
2024-11-06 07:58:05 -0300

    Aula 02

---
+++ fundamentos/2-tipos.rb
@@ -0,0 +1,9 @@
+# Dados do Filme
+
+name = "Avatar"
+year_launch = 2009
+price = 100.00
+plan_included = false
+
+puts name, year_launch
+puts "name: #{name.class}, year: #{year_launch.class}, price: #{price.class}, plan: #{plan_included.class}"
