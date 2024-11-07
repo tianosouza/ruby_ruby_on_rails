c1d69dfc976cb08ba33193a72912c4eafa6b87a1
Cristiano Souza <ltianosouzal@outlook.com>
2024-11-07 11:53:34 -0300

    Aula 13

---
+++ fundamentos/13-metodos.rb
@@ -0,0 +1,30 @@
+# 1- Método para imprimir Hello world
+
+def hello
+  puts "hello world!"
+end
+
+hello
+
+# 2- Método para somar dois números
+
+def sum(num1, num2)
+  puts num1 + num2
+end
+
+sum(10,20)
+
+# 3- Método para cadatrar Filmes
+
+def create_movie
+  puts "Informe o nome do filme: "
+  name = gets.chomp
+  puts "Informe o ano de lançament0: "
+  year_launch = gets.chomp.to_i 
+  puts "Informe o preço do filme: "
+  price = gets.chomp.to_f
+
+  puts "#{name} - R$ #{price}"
+end
+
+create_movie
