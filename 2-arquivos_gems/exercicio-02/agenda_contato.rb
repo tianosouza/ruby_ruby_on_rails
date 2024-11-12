9f885fae838f0a281a8e6666fe677d876c352f9f
Cristiano Souza <ltianosouzal@outlook.com>
2024-11-12 07:24:32 -0300

    Exercicio 02 csv

---
+++ 2-arquivos_gems/exercicio-02/agenda_contato.rb
@@ -0,0 +1,21 @@
+=begin
+  Escreva um programa Ruby para lê três variáveis: nome, idade, cidade.
+  A despeito desse valores você deve utiliza-los para alimentar uma planilha
+  de conttos e posteriormente, salvar o arquivo com o nome contatatos.csv
+=end
+
+require 'csv'
+
+puts '====AGENDA DE CONTATOS===='
+puts 'Digite o nome do contato: '
+nome = gets.chomp
+puts 'Digite a idade do contato: '
+idade = gets.chomp.to_i
+puts 'Digite a cidade do contato: '
+cidade = gets.chomp
+
+
+CSV.open('../data/contatos.csv', 'w') do |csv|
+  csv << ['Nome', 'idade', 'cidade']
+  csv << [nome, idade, cidade]
+end
